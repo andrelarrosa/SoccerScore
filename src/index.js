aa3baaee535a6314f555fa8909d79fab1cca67cc
André Larrosa <andre_luiz_2001@hotmail.com>
2020-11-21 13:14:44 -0300

    fixed the bug when your team are duplicated in the result, example, your team equals palmeiras, and the result returned is 'palmeiras 0 x 0 palmeiras'

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const readline = require('readline-sync')
 async function resultadoJogo(){
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
-    const yourTeam = readline.question("Informe o time que você quer obter informações: ")
-    const URL = `https://www.google.com/search?sxsrf=ALeKk00vj8B6I_dJfY5UKTzx_lds3cGsHQ%3A1604538696663&ei=SFGjX8mKKJeu5OUPoZ-UYA&q=${yourTeam}+ultimo+jogo`
+    var yourTeam = readline.question("Informe o time que você quer obter informações: ")
+    const URL = `https://www.google.com/search?sxsrf=ALeKk00vj8B6I_dJfY5UKTzx_lds3cGsHQ%3A1604538696663&ei=SFGjX8mKKJeu5OUPoZ-UYA&q=${yourTeam}`
     await page.goto(URL)
     
     const socreYourTeam = await page.evaluate(()=>{
@@ -21,6 +21,13 @@ async function resultadoJogo(){
         var a = document.querySelector(".imso_mh__second-tn-ed.imso_mh__tnal-cont.imso-tnol div.imso_mh__tm-nm.imso-medium-font.imso_mh__tm-nm-ew div.ellipsisize.liveresults-sports-immersive__team-name-width.kno-fb-ctx span").innerHTML
         return a;
     })
+    if(yourTeam.toUpperCase == opposingTeam.toUpperCase){
+        const team = await page.evaluate(()=>{
+            var a = document.querySelector(".ellipsisize.liveresults-sports-immersive__team-name-width.kno-fb-ctx span").innerHTML
+            return a 
+        })
+        yourTeam = team
+    }
     console.log(`${yourTeam} ${socreYourTeam} x ${scoreOpposingTeam} ${opposingTeam}`)
 
     await browser.close();
